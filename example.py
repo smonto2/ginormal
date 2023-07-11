8d7acee667823c273587584eb422bb708d8ac0ce
Santiago Montoya <smontoya@outlook.com>
2023-07-11 14:28:49 +0100

    Finalized warnings, documentation and example

--- example.py
+++ example.py
@@ -14,16 +14,19 @@ t1 = 1
 t2 = 2
 
 # Choose the configuration of values you want to plot
+n_grid = 200
 a = a2
 m = m1
 t = t1
-z_vals = np.linspace(-5, 5, 200)
+z_vals = np.linspace(-5, 5, n_grid)
 fz_unc = np.array([gin.dgin(z, a, m, t, False) for z in z_vals])
-fz_p = np.array([gin.dtgin(z, a, m, t, True, False) for z in z_vals])
-fz_n = np.array([gin.dtgin(z, a, m, t, False, False) for z in z_vals])
+fz_p = np.array([gin.dtgin(z, a, m, t, True, False) for z in z_vals[z_vals > 0]])
+fz_p = np.append(np.zeros(n_grid - len(fz_p)), fz_p)
+fz_n = np.array([gin.dtgin(z, a, m, t, False, False) for z in z_vals[z_vals < 0]])
+fz_n = np.append(fz_n, np.zeros(n_grid - len(fz_n)))
 z_unc = gin.rgin(n_draws, a, m, t, 'hormann')
 z_p = gin.rtgin(n_draws, a, m, t, True, 'hormann')
-z_n = gin.rtgin(n_draws, a, m, t, True, 'leydold')
+z_n = gin.rtgin(n_draws, a, m, t, False, 'hormann')
 
 # Full density on real line
 plt.figure()
